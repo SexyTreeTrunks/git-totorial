5e53d342f9a76e248806890828f9c94a1b458701
minjeong <apenr1234@gmail.com>
2018-08-02 20:08:39 +0900

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("-----수정할 코드-----");		
+		System.out.println("이민정");
+		System.out.println("강명구");
+		System.out.println("정윤식");
+		System.out.println("유나");
+		System.out.println("김용현");
+
+		System.out.println("-----새로 작성한 코드-----");	
+		
+		System.out.println("-----삭제할 코드-----");		
+		System.out.println("이민정 - 삭제할 코드");
+		System.out.println("강명구 - 삭제할 코드");
+		System.out.println("정윤식 - 삭제할 코드");
+		System.out.println("유나 - 삭제할 코드");
+		System.out.println("김용현 - 삭제할 코드");
+	}
+}
