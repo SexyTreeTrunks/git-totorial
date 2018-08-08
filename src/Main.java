adb85726b7b6ab8ddb33fd3ac350eab9fa7290d4
kimyonghyeon <dydgus828@naver.com>
2018-08-08 18:12:13 +0900

    코드수정

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
 		System.out.println("-----새로 작성한 코드-----");	
 		System.out.println("강명구 수정 코드");
+		System.out.println("김용현 수정 코드");
 		
 		System.out.println("-----삭제할 코드-----");		
 		System.out.println("이민정 - 삭제할 코드");
