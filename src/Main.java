7c27e0d6bcd929bd8467f2a179fcdab5b05d1e5a
yoooooona <chldbsk8911@naver.com>
2018-08-09 18:40:13 +0900

    새로작성한코드

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,13 @@ public class Main {
 		System.out.println("정윤식");
 		System.out.println("유나-수정    코드");
 		System.out.println("김용현");
-
+                
 		System.out.println("-----새로 작성한 코드-----");	
 		System.out.println("강명구 수정 코드");
 		System.out.println("이민정 - 새로 작성한 코드");
-		
+		System.out.println("최유나 - 수정 코드");
+
+
 		System.out.println("-----삭제할 코드-----");
 		System.out.println("정윤식 - 삭제할 코드");
 		System.out.println("유나 - 삭제할 코드");
